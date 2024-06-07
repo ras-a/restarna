af561f023027be2431eb345ffbdf57ed6033b122
Rasmus <r.andreassen.tisdk.2024@gmail.com>
2024-06-07 13:26:39 +0900

    first commit

---
+++ src/test/java/jp/co/creambakery/restarna/RestarnaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.creambakery.restarna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestarnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
