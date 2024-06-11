4343bb72e394b9e4ccb3e3714f39a84b6803654a
Rasmus <r.andreassen.tisdk.2024@gmail.com>
2024-06-11 13:24:55 +0900

    実装ミスを修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,72 @@ INSERT INTO bread(id, name, reading, price, description, image, date_created) VA
 INSERT INTO cream(id, name, reading, price, description, image, date_created) VALUES (cream_seq.nextval, '餡子', 'あんこ', 80, '国産の小豆で作っています', 'anko.jpeg', CURRENT_DATE);
 INSERT INTO item(id, name, reading, bread, description)  VALUES (item_seq.nextval, 'あんパン', 'あんぱん', (SELECT id FROM bread WHERE name = 'バンズ'), '町のあんパン');
 INSERT INTO item_cream VALUES (1, 1);
+
+INSERT INTO customer(
+	id,
+	name,
+	reading,
+	password,
+	email
+) VALUES (
+	1,
+	'太郎',
+	'たろう',
+	'taroo',
+	'e@mail.com'
+);
+
+
+INSERT INTO address_profile(
+	id,
+	customer,
+	name,
+	post_code,
+	address,
+	addressee_name,
+	addressee_reading
+) VALUES (
+	1,
+	1,
+	'main',
+	'000-000',
+	'京都府京都市',
+	'太郎',
+	'たろう'
+);
+
+INSERT INTO credit_card(
+	id,
+	owner,
+	name,
+	holder_name,
+	no,
+	cvc
+) VALUES (
+	1,
+	1,
+	'card',
+	'CREAM TARO',
+	'0123456789101112',
+	'123'
+);
+
+INSERT INTO product_order(
+	id,
+	customer,
+	payment_method,
+	credit_card,
+	address
+) VALUES (
+	1,
+	1,
+	0,
+	1,
+	1
+);
+
+INSERT INTO product_order_item VALUES (
+	1,
+	1,
+	1
+);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE customer (
 	reading VARCHAR2(64 CHAR) NOT NULL,
 	password VARCHAR2(128) NOT NULL,
 	email VARCHAR2(64 CHAR) NOT NULL,
-	date_created DATE NOT NULL,
+	date_created DATE DEFAULT CURRENT_DATE,
 	deleted NUMBER(1) DEFAULT 0
 );
 
@@ -13,14 +13,14 @@ CREATE SEQUENCE customer_seq NOCACHE;
 CREATE TABLE address_profile (
 	id INTEGER PRIMARY KEY,
 	customer INTEGER REFERENCES customer(id) NOT NULL,
-	name VARCHAR2(32 CHAR) NOT NULL,
+	name VARCHAR2(32 CHAR) UNIQUE NOT NULL,
 	post_code VARCHAR2(8) NOT NULL,
 	address VARCHAR2(128 CHAR) NOT NULL,
 	phone_number VARCHAR(11),
 	email VARCHAR2(64 CHAR),
 	addressee_name VARCHAR2(32 CHAR) NOT NULL,
 	addressee_reading VARCHAR(64 CHAR) NOT NULL,
-	date_created DATE NOT NULL,
+	date_created DATE DEFAULT CURRENT_DATE,
 	deleted NUMBER(1) DEFAULT 0
 );
 
@@ -35,7 +35,7 @@ CREATE TABLE bread (
 	price INTEGER NOT NULL,
 	description VARCHAR2(128 CHAR) NOT NULL,
 	image VARCHAR2(64 CHAR) NOT NULL,
-	date_created DATE NOT NULL,
+	date_created DATE DEFAULT CURRENT_DATE,
 	deleted NUMBER(1) DEFAULT 0
 );
 
@@ -48,7 +48,7 @@ CREATE TABLE cream (
 	price INTEGER NOT NULL,
 	description VARCHAR2(128 CHAR) NOT NULL,
 	image VARCHAR2(64 CHAR) NOT NULL,
-	date_created DATE NOT NULL,
+	date_created DATE DEFAULT CURRENT_DATE,
 	deleted NUMBER(1) DEFAULT 0
 );
 
@@ -89,25 +89,24 @@ CREATE TABLE favorite (
 );
 
 CREATE TABLE credit_card (
+	id INTEGER PRIMARY KEY,
 	owner INTEGER REFERENCES customer(id) NOT NULL,
 	name VARCHAR2(32 CHAR) NOT NULL,
 	holder_name VARCHAR2(64) NOT NULL,
 	no VARCHAR2(16) NOT NULL,
-	cvc VARCHAR2(3) NOT NULL,
-	CONSTRAINT pk_cc PRIMARY KEY (owner, name)
+	cvc VARCHAR2(3) NOT NULL
 );
 
 CREATE TABLE product_order (
 	id INTEGER PRIMARY KEY,
 	customer INTEGER REFERENCES customer(id),
-	payment_method NUMBER(1) NOT NULL,
-	credit_card VARCHAR2(32 CHAR),
-	date_created DATE NOT NULL,
+	payment_method NUMBER(1) NOT NULL CHECK (payment_method BETWEEN 0 AND 4),
+	credit_card INTEGER CHECK ((payment_method = 0 AND credit_card IS NOT NULL) OR payment_method <> 0) REFERENCES credit_card(id),
+	date_created DATE DEFAULT CURRENT_DATE,
 	cancelled NUMBER(1) DEFAULT 0,
 	address INTEGER REFERENCES address_profile(id) NOT NULL,
 	optional_details VARCHAR2(512),
-	completed DATE,
-	FOREIGN KEY (customer, credit_card) REFERENCES credit_card(owner, name)
+	completed DATE
 );
 
 CREATE SEQUENCE order_seq NOCACHE;
@@ -123,7 +122,7 @@ CREATE TABLE admin (
 	id INTEGER PRIMARY KEY,
 	name VARCHAR2(16 CHAR),
 	password VARCHAR2(32),
-	date_created DATE NOT NULL,
+	date_created DATE DEFAULT CURRENT_DATE,
 	deleted NUMBER(1) DEFAULT 0,
 	system NUMBER(1) DEFAULT 0
 );
