4ca157f1514d093c8f14b6268aa31147b063d885
ShionKiriyama <>
2024-06-11 16:55:17 +0900

    ProductOrder編集

--- src/main/java/jp/co/creambakery/entity/ProductOrder.java
+++ src/main/java/jp/co/creambakery/entity/ProductOrder.java
@@ -2,15 +2,7 @@ package jp.co.creambakery.entity;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 
 /**
  * product_orderテーブルのエンティティ
