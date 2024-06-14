f5fcb4a7a15b0dbb05e69bd6f5c8509d6521a23e
u <h.ueda.tisdx.2024@gmail.com>
2024-06-14 11:47:37 +0900

    【クリーム一覧と全表示コントローラーの作成】

--- src/main/java/jp/co/creambakery/controller/item/ListController.java
+++ src/main/java/jp/co/creambakery/controller/item/ListController.java
@@ -1,8 +1,30 @@
 package jp.co.creambakery.controller.item;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import jp.co.creambakery.repository.*;
 
 @Controller
-public class ListController {
+public class ListController
+{
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    CreamRepository creamRepository;
 
+    @GetMapping("/list")
+    public String home(Model model)
+    {
+        model.addAttribute("items", itemRepository.findAll());
+        return "item/test";
+    }
+
+    @GetMapping("/cream/list")
+    public String cream(Model model)
+    {
+        model.addAttribute("creams", creamRepository.findAll());
+        return "item/test";
+    }
 }
