7965eec865135177f662c31dd74dfd31129dcf5f
市田　雄也 <y.ichida.tisdx.202404@gmail.com>
2024-06-17 09:38:22 +0900

    LoginController(Customer + Admin)を作成

--- src/main/java/jp/co/creambakery/controller/admin/LoginController.java
+++ src/main/java/jp/co/creambakery/controller/admin/LoginController.java
@@ -1,9 +1,60 @@
 package jp.co.creambakery.controller.admin;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.*;
+import jakarta.validation.*;
+import jp.co.creambakery.entity.*;
+import jp.co.creambakery.form.*;
+import jp.co.creambakery.repository.*;
 
 @Controller
 @Component("adminlogin")
-public class LoginController {
+@RequestMapping("/login")
+public class LoginController 
+{
 
+    @Autowired
+	private AdminRepository repository;
+    @Autowired
+    HttpSession session;
+
+    @GetMapping
+    public String adminLogin(@ModelAttribute AdminLoginForm form, Model model) 
+    {
+        session.invalidate();
+        model.addAttribute("form", form);
+        return "admin/login";
+    }
+
+    @PostMapping
+    public String adminLogin(@Valid @ModelAttribute AdminLoginForm form,
+                            BindingResult result, HttpSession session, Model model) 
+    {
+
+        if(result.hasErrors()) 
+        {
+            model.addAttribute("errMessage", "名前、またはパスワードが間違っています。");
+            model.addAttribute("form", form);
+            return "admin/login";
+        }
+        String name = form.getName();
+        String password = form.getPassword();
+
+        Admin admin = repository.findByNameAndPassword(name, password);
+        if (admin != null) 
+        {
+            session.setAttribute("adminId", admin.getId());
+            session.setAttribute("adminName", admin.getName());
+            return "redirect:/admin/";
+        }else
+        {
+            model.addAttribute("errMessage", "名前、またはパスワードが間違っています。");
+            return "admin/login";
+        }   
+    }
 }
--- src/main/java/jp/co/creambakery/controller/session/LoginController.java
+++ src/main/java/jp/co/creambakery/controller/session/LoginController.java
@@ -1,10 +1,58 @@
 package jp.co.creambakery.controller.session;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.*;
+import jakarta.validation.*;
+import jp.co.creambakery.entity.*;
+import jp.co.creambakery.form.*;
+import jp.co.creambakery.repository.*;
 
 @Controller
 @Component("Userlogin")
-public class LoginController {
+@RequestMapping("/login")
+public class LoginController
+{
+    @Autowired
+	private CustomerRepository repository;
+    @Autowired
+    HttpSession session;
 
+    @GetMapping
+    public String userLogin(@ModelAttribute UserLoginForm form, Model model)
+    {
+        session.invalidate();
+        model.addAttribute("form", form);
+        return "client/login";
+    }
+
+    @PostMapping
+    public String userLogin(@Valid @ModelAttribute UserLoginForm form,
+                            BindingResult result, Model model) 
+    {
+        if(result.hasErrors()) 
+        {
+            model.addAttribute("form", form);
+            model.addAttribute("errMessage", "メールアドレス、またはパスワードが間違っています。");
+            return "client/login";
+        }
+        String email = form.getEmail();
+        String password = form.getPassword();
+        Customer customer = repository.findByEmailAndPassword(email, password);
+        
+        if (customer != null)
+        {
+            session.setAttribute("id", customer.getId());
+            session.setAttribute("name", customer.getName());
+            return "redirect:/session/home";
+        }else
+        {
+            model.addAttribute("errMessage", "メールアドレス、またはパスワードが間違っています。");
+            return "client/login";
+        }
+    }
 }
